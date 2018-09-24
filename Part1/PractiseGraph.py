2a9a2e9fea3091966c200801dabed9fb2d30ab63
srimani-programmer <srimani.scholar@gmail.com>
2018-09-24 21:46:44 +0530

    Graph-01

---
+++ Part1/PractiseGraph.py
@@ -0,0 +1,23 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+g = nx.Graph()
+
+g.add_nodes_from([1,2,3,4,5,6,7,8,9,10,11,12])
+
+g.add_edge(1,2)
+g.add_edge(2,3)
+g.add_edge(3,4)
+g.add_edge(4,5)
+g.add_edge(5,6)
+g.add_edge(5,12)
+g.add_edge(7,8)
+g.add_edge(8,9)
+g.add_edge(9,10)
+g.add_edge(10,11)
+g.add_edge(11,12)
+g.add_edge(12,1)
+
+nx.draw(g)
+
+plt.show()
